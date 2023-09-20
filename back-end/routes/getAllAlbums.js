a04c26eb66e571f11fcc8c8ad2c9f1e1120591c4
TheDanishMexican <danieljchristjansen@gmail.com>
2023-09-20 13:53:19 +0200

    getAllArtists and getAllAlbums work now due to fixed queiries

--- back-end/routes/getAllAlbums.js
+++ back-end/routes/getAllAlbums.js
@@ -7,12 +7,21 @@ const allAlbums = express.Router();
 
 allAlbums.get('/albums', (request, response) => {
     const query = `
-        SELECT albums.album_id, albums.album_name, artists.artist_name
-        FROM albums
-        INNER JOIN artists ON albums.artist_id = artists.artist_id
-        ORDER BY albums.album_name;
-    `;
-
+        SELECT
+            albums.album_name,
+            GROUP_CONCAT(DISTINCT artists.artist_name ORDER BY artists.artist_name ASC) AS artist_names,
+            GROUP_CONCAT(tracks.track_name ORDER BY tracks.track_name ASC) AS track_names
+        FROM
+            albums
+        INNER JOIN
+            track_album ON albums.album_id = track_album.album_id
+        INNER JOIN
+            tracks ON track_album.track_id = tracks.track_id
+        INNER JOIN
+            artists ON tracks.artist_id = artists.artist_id
+        GROUP BY
+            albums.album_name;
+        `
     database.query(query, (error, results, fields) => {
         if (error) {
             console.log(error);
--- back-end/routes/getAllTracks.js
+++ back-end/routes/getAllTracks.js
@@ -8,12 +8,23 @@ const allTracks = express.Router();
 //Get all tracks
 allTracks.get('/tracks', (request, response) => {
     const query = `
-        SELECT tracks.track_id, tracks.track_name, artists.artist_name, albums.album_name
-        FROM tracks
-        INNER JOIN artists ON tracks.artist_id = artists.artist_id
-        INNER JOIN albums ON tracks.album_id = albums.album_id
-        ORDER BY tracks.track_name;
-    `;
+        SELECT
+        tracks.track_name,
+        GROUP_CONCAT(DISTINCT albums.album_name ORDER BY albums.album_name ASC) AS album_names,
+        GROUP_CONCAT(DISTINCT artists.artist_name ORDER BY artists.artist_name ASC) AS artist_names
+        FROM
+            tracks
+        INNER JOIN
+            track_album ON tracks.track_id = track_album.track_id
+        INNER JOIN
+            albums ON track_album.album_id = albums.album_id
+        INNER JOIN
+            track_artist ON tracks.track_id = track_artist.track_id
+        INNER JOIN
+            artists ON track_artist.artist_id = artists.artist_id
+        GROUP BY
+            tracks.track_name;
+    `
 
     database.query(query, (error, results, fields) => {
         if (error) {
