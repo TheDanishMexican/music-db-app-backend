be96e3baa35e2d2f53fb3d5950b190212b2be2a6
TheDanishMexican <danieljchristjansen@gmail.com>
2023-09-22 11:42:15 +0200

    search route works

--- routes/search.js
+++ routes/search.js
@@ -7,19 +7,57 @@ const search = express.Router();
 
 search.get('/search', (request, response) => {
     const searchTerm = `%${request.query.q}%`;
-    const values = [searchTerm, searchTerm, searchTerm];
-    const query = `SELECT tracks.track_id, tracks.track_name, artists.artist_name AS track_artist_name, 
-            albums.album_id, albums.album_name, artists2.artist_name AS album_artist_name
-            FROM tracks
-            LEFT JOIN artists ON tracks.artist_id = artists.artist_id
-            LEFT JOIN albums ON tracks.album_id = albums.album_id
-            LEFT JOIN artists AS artists2 ON albums.artist_id = artists2.artist_id
-            WHERE tracks.track_name LIKE ? OR
-            artists.artist_name LIKE ? OR
-            albums.album_name LIKE ?;
-        `;
+    const values = [searchTerm, searchTerm];
+    const query = `
+    (
+    SELECT
+        'album' AS result_type,
+        albums.album_id AS id,
+        albums.album_name AS name,
+        GROUP_CONCAT(DISTINCT artists.artist_name SEPARATOR ', ') AS artist_names,
+        GROUP_CONCAT(DISTINCT tracks.track_name SEPARATOR ', ') AS track_names
+    FROM
+        albums
+    INNER JOIN
+        track_album ON albums.album_id = track_album.album_id
+    INNER JOIN
+        tracks ON track_album.track_id = tracks.track_id
+    INNER JOIN
+        artists ON tracks.artist_id = artists.artist_id
+    WHERE
+        albums.album_name LIKE '${searchTerm}'
+    GROUP BY
+        albums.album_id, albums.album_name
 
+)
+UNION
+(
+    SELECT
+        'track' AS result_type,
+        tracks.track_id AS id,
+        tracks.track_name AS name,
+        GROUP_CONCAT(DISTINCT albums.album_name ORDER BY albums.album_name ASC) AS album_names,
+        GROUP_CONCAT(DISTINCT artists.artist_name SEPARATOR ', ') AS artist_names
+    FROM
+        tracks
+    INNER JOIN
+        track_album ON tracks.track_id = track_album.track_id
+    INNER JOIN
+        albums ON track_album.album_id = albums.album_id
+    INNER JOIN
+        track_artist ON tracks.track_id = track_artist.track_id
+    INNER JOIN
+        artists ON track_artist.artist_id = artists.artist_id
+    WHERE
+        tracks.track_name LIKE '${searchTerm}'
+    GROUP BY
+        tracks.track_id, tracks.track_name
+)
+ORDER BY
+    result_type, name;
 
+    `
+console.log(query);
     database.query(query, values, (error, results, fields) => {
         if (error) {
             console.log(error);
