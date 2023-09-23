b29ca7af5619ab4f2fcc211ca9145cc2057973a2
TheDanishMexican <danieljchristjansen@gmail.com>
2023-09-23 21:04:11 +0200

    added Asbjorns getAlbumInfo

---
+++ routes/getAlbumInfo.js
@@ -0,0 +1,52 @@
+"use strict"
+
+import express from "express"
+import database from "../database.js"
+
+const getAlbumInfo = express.Router();
+
+getAlbumInfo.get(`/albums/:id`, (request, response) => {
+    const id = request.params.id;
+
+    const query =`
+SELECT 
+    albums.album_id, 
+    albums.album_name, 
+    tracks.track_id, 
+    tracks.track_name
+FROM 
+    albums
+LEFT JOIN 
+    tracks ON albums.album_id = tracks.album_id
+WHERE 
+    albums.album_id = ?;
+
+
+            `
+
+    const values = [id];
+
+    database.query(query, values, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+            response.status(500).json({ message: 'Fejl ved hentning af albuminfo' });
+        } else {
+            if (results.length === 0) {
+                response.status(404).json({ message: 'Album blev ikke fundet' });
+            } else {
+                const albumInfo = {
+                    album_id: results[0].album_id,
+                    album_name: results[0].album_name,
+                    artist_name: results[0].artist_name,
+                    tracks: results.map(track => ({
+                        track_id: track.track_id,
+                        track_name: track.track_name
+                    }))
+                };
+                response.json(albumInfo);
+            }
+        }
+    });
+});
+
+export default getAlbumInfo
--- server.js
+++ server.js
@@ -10,13 +10,13 @@ import allArtists from './routes/getAllArtists.js';
 import createArtist from './routes/createArtist.js';
 import allAlbums from './routes/getAllAlbums.js';
 import createAlbum from './routes/createAlbum.js';
-import getOneAlbum from './routes/getOneAlbum.js';
 import search from './routes/search.js';
 import addArtistToTrack from './routes/addArtistToTrack.js';
 import addTrackToAlbum from './routes/addTrackToAlbum.js';
 import deleteAlbum from './routes/deleteAlbum.js';
 import getUsers from './routes/getUsersTest.js';
 import frontPage from './routes/frontPage.js';
+import getAlbumInfo from './routes/getAlbumInfo.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,13 +33,13 @@ app.use('/', allArtists);
 app.use('/', createArtist);
 app.use('/', allAlbums);
 app.use('/', createAlbum);
-app.use('/', getOneAlbum);
 app.use('/', search);
 app.use('/', addArtistToTrack);
 app.use('/', addTrackToAlbum);
 app.use('/', deleteAlbum);
 app.use('/', getUsers);
 app.use('/', frontPage);
+app.use('/', getAlbumInfo);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
